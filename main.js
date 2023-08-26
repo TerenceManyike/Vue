1106fb064149c725c47f12e2f07db82882cb5cae
Sanmanyike <terrencemanyike@gmail.com>
2023-08-26 21:40:56 +0200

    Update: Improve my vue app

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ const app = Vue.createApp({
             product: 'Socks',
             image: 'assets/images/socks_blue.jpg',
             availability: true,
+            options: [
+                {
+                    colour: "blue", image: "assets/images/blue.png", image_url: "assets/images/socks_blue.jpg", alt: "blue"
+                },
+                {
+                    colour: "green", image: "assets/images/green.png", image_url: "assets/images/socks_green.jpg", alt: "green"
+                }
+            ]
+        }
+    },
+    methods:{
+        alterImage(optionsImage){
+            this.image = optionsImage;
         }
     }
 })
